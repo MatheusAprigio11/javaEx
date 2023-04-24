9f9b84b78aa3514e4dc450a660072f157c6758ac
Matheus Aprigio <126816425+MatheusAprigio11@users.noreply.github.com>
2023-04-24 17:00:20 -0300

    Create trains.java

---
+++ trains.java
@@ -0,0 +1,45 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class trains {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int maxRange = 0;
+        int positionA;
+        int positionB;
+        int spdA;
+        int spdB;
+
+        while (true) {
+            System.out.println("Input the first position in KM of the train A>> ");
+            positionA = sc.nextInt();
+            try{
+                if (positionA > 10000) {
+                    continue;
+                    }
+                }catch (InputMismatchException e) {
+                System.out.println("The number must to be bigger or equal than zero and less than 10000.");
+            }
+
+            System.out.println("Input the speed of the train A>> ");
+            spdA = sc.nextInt();
+            try{
+                if spdA 
+            }
+
+            System.out.println("Now input the first position in KM of the train B>> ");
+            positionB = sc.nextInt();
+                try{
+                    if (positionB > 10000) {
+                        continue;
+                    }
+                }catch (InputMismatchException e) {
+                    System.out.println("The number must to be bigger or equal than zero and less than 10000.");
+                }
+            System.out.println("Input the speed of the train B>> ");
+            spdB = sc.nextInt();
+
+
+        }
+    }
+}
